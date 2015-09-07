55b6f38aa2a198eb09efb2e8e041f80d0a24f659
Paula Waite <pawaite@madisoncollege.edu>
2015-09-07 14:39:51 -0500

    "Week 1 code"

---
+++ src/java111/project1/demo/Sample.java
@@ -0,0 +1,14 @@
+/** Our first class to demonstrate compiling and running
+ * 
+ * @author Paula Waite
+ */
+ 
+ public class Sample {
+ 
+     public static void main(String[] args) {
+         System.out.println("some text to see on the screen");
+     }
+ 
+ 
+ }
+ 
